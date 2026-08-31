5aafefd3ad71db4cce9b3e0bbb48705582f28123
Felipe Sampaio <felplilp@gmail.com>
2026-08-31 20:50:49 -0300

    remove debug comments

--- src/AppBikeTerminal.java
+++ src/AppBikeTerminal.java
@@ -55,7 +55,5 @@ public class AppBikeTerminal {
         scanner.close();
 
         estudante.visualizarHistorico();
-        estacaoPrincipal.listStatus();
-        estacaoSecundaria.listStatus();
     }
 }
